9df834d6dafde71d6665b360b631f4e1b6a2c592
Singhasha1 <112199624+Singhasha1@users.noreply.github.com>
2022-10-06 15:30:49 +0530

    Updates

---
+++ precache-manifest.fcc93fb3597de9d92de9cdcdb9f14394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f8fb91c397a24b413cc",
+    "url": "/text-utility/static/css/main.69c04d5e.chunk.css"
+  },
+  {
+    "revision": "0f8fb91c397a24b413cc",
+    "url": "/text-utility/static/js/main.0f8fb91c.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/text-utility/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1fb84335d4ce8f26b1b6",
+    "url": "/text-utility/static/js/2.1fb84335.chunk.js"
+  },
+  {
+    "revision": "326b7c3ed529ad54513e",
+    "url": "/text-utility/static/js/runtime~main.326b7c3e.js"
+  },
+  {
+    "revision": "fe58a1321cbd593422f278fa1652e1d0",
+    "url": "/text-utility/index.html"
+  }
+];
